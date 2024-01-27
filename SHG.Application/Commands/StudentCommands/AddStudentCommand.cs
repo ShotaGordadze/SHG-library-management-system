eb078ae60de6f2c39bfaa33ecc97ded5e9c38324
Shota Gordadze <shota.gordadze.3@gmail.com>
2024-01-27 20:16:00 +0400

    add student command

---
+++ SHG.Application/Commands/StudentCommands/AddStudentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SHG.Application.Dtos;
+
+namespace SHG.Application.Commands.StudentCommands;
+
+public record AddStudentCommand() : IRequest<StudentDto>;
+
+public class AddStudentCommandHandler : IRequestHandler<AddStudentCommand, StudentDto>
+{
+    public Task<StudentDto> Handle(AddStudentCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+

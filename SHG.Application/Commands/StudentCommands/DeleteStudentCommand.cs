83b065546f6beacda4e03000429e7792087f27bf
Shota Gordadze <shota.gordadze.3@gmail.com>
2024-01-27 20:20:54 +0400

    DeleteStudent command

---
+++ SHG.Application/Commands/StudentCommands/DeleteStudentCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SHG.Application.Dtos;
+using SHG.Infrastructure;
+using SHG.Infrastructure.Repositories;
+
+namespace SHG.Application.Commands.StudentCommands;
+
+public record DeleteStudentCommand(int id) : IRequest<int>;
+
+public class DeleteStudentCommandHandler : IRequestHandler<DeleteStudentCommand, int>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IStudentRepository _studentRepository;
+
+    public DeleteStudentCommandHandler(IUnitOfWork unitOfWork, IStudentRepository studentRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _studentRepository = studentRepository;
+    }
+
+    public async Task<int> Handle(DeleteStudentCommand request, CancellationToken cancellationToken)
+    {
+        var student = await _studentRepository.Find(request.id);
+
+        if (student == null)
+        {
+            return -1;
+        }
+
+        _studentRepository.Delete(student);
+        await _unitOfWork.SaveAsync(cancellationToken);
+
+        return request.id;
+    }
+}
+
+

752bac6e9f16016a760122fd955d47b735f76a04
Shota Gordadze <shota.gordadze.3@gmail.com>
2024-01-29 15:26:58 +0400

    Added FindStudent query

---
+++ SHG.Application/Queries/FindStudentQuery.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SHG.Application.Dtos;
+using SHG.Infrastructure.Repositories;
+
+namespace SHG.Application.Queries;
+
+public record FindStudentQuery(int Id) : IRequest<StudentDto>;
+
+public class FindStudentQueryHandler : IRequestHandler<FindStudentQuery, StudentDto>
+{
+    private readonly IStudentRepository _studentRepository;
+
+    public FindStudentQueryHandler(IStudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
+    public async Task<StudentDto> Handle(FindStudentQuery request, CancellationToken cancellationToken)
+    {
+        var student = await _studentRepository.Query(x => x.Id == request.Id)
+                                              .FirstAsync(cancellationToken);
+
+        return new StudentDto
+        {
+            Id = student.Id,
+            Name = student.Name,
+            Email = student.Email,
+            Books = student.Books,
+        };
+    }
+}
+
+

b53cb6abadf67d25491058544f9ce74dfed96150
Shota Gordadze <shota.gordadze.3@gmail.com>
2024-01-30 20:45:33 +0400

    models

---
+++ SHG.Api/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SHG.Api.Controllers;
+
+[ApiController]
+[Route("Controller")]
+public class StudentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public StudentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+}
---
+++ SHG.Api/Models/Models.cs
@@ -0,0 +1,9 @@
+﻿namespace SHG.Api.Models;
+
+public record AuthorModel(string Name, string Lastname);
+
+public record StudentModel(string Name, string Email);
+
+public record BookModel(string Name, string Email);
+
+public record CategoryModel(string Name, string? Description);
